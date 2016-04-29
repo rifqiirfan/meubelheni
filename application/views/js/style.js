dfc140b3849344f12370af2baa6a9e4e2d94ea97
rifqiirfan <rifqiirfan@gmail.com>
2016-04-29 16:00:18 +0700

    folder css, js, dan img

---
+++ application/views/js/style.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+    autoPlay : 3000,
+    stopOnHover : true,
+    navigation:true,
+    paginationSpeed : 1000,
+    goToFirstSpeed : 2000,
+    singleItem : true,
+    autoHeight : true,
+    transitionStyle:"goDown"
+});
+});
