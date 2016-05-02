8dbfe62c901d1877779bbd8bc8796f68d0e1a358
rifqiirfan <rifqiirfan@gmail.com>
2016-05-02 12:04:53 +0700

    update css

--- application/views/login.php
+++ application/views/login.php
@@ -3,8 +3,8 @@
     <title>Login || Mebel Heni</title>
    <meta charset="UTF=8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
-    <link rel="stylesheet" type="text/css" href="css/style.css" >
+    <link rel="stylesheet" type="text/css" href="<?=base_url();?>css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="<?=base_url();?>css/style.css" >
     <link rel="shortcut icon" href="img/mebelheny.ico"-->
 </head>
 <body>
