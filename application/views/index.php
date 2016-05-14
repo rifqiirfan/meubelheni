f407b10bb24fc53e030da78d6e7e78cffa32f590
rifqiirfan <rifqiirfan@gmail.com>
2016-05-14 11:20:04 +0700

    php

--- application/views/index.php
+++ application/views/index.php
@@ -22,12 +22,6 @@
 		</div>
 		<div id="menu">
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
 				<nav><ul>
 					<li class="current_page"><a href="home">Halaman Depan</a></li>
 					<li><a href="#">Tentang Kami</a></li>
