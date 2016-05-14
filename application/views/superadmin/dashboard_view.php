10ad658c3d62899d9f225baa339db7c31291ac50
rifqiirfan <rifqiirfan@gmail.com>
2016-05-14 12:06:01 +0700

    php

--- application/views/superadmin/dashboard_view.php
+++ application/views/superadmin/dashboard_view.php
@@ -30,11 +30,6 @@
   <nav class="navbar navbar-inverse visible-xs">
     <div class="container-fluid">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a class="navbar-brand" href="#">Mebel Heni</a>
       </div>
       <div class="collapse navbar-collapse" id="myNavbar">
