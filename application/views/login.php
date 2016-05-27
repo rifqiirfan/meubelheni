5b25a2436be0528a0765f675095d6b5ff669f6c9
rifqiirfan <rifqiirfan@gmail.com>
2016-05-27 09:53:45 +0700

    changes on login.php

--- application/views/login.php
+++ application/views/login.php
@@ -1,44 +1,44 @@
 <html>
 <head>
-    <title>Login || Mebel Heni</title>
-    <meta charset="UTF=8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap.min.css" >
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/style.css" >
-    <link rel="shortcut icon" href="<?=base_url();?>assets/img/mebelheny.ico"-->
+  <title>Login || Mebel Heni</title>
+  <meta charset="UTF=8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/bootstrap.min.css" >
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/assets/css/style.css" >
+  <link rel="shortcut icon" href="<?=base_url();?>assets/img/mebelheny.ico"-->
 </head>
 <body>
-    <div class="header" id="container">
-        <div id="logo">
-            <a href="../home"><img src="<?=base_url();?>assets/img/mebelheny.png" height="60px"></a>
-        </div>
-        <div id="menu">
-            <nav><ul>
-                <li><a href="home">Halaman Depan</a></li>
-                <li><a href="#">Tentang Kami</a></li>
-                <li class="current_page"><a href="login">Login</a></li>
-            </ul></nav>
-        </div>
+  <div class="header" id="container">
+    <div id="logo">
+      <a href="../home"><img src="<?=base_url();?>assets/img/mebelheny.png" height="60px"></a>
     </div>
-    <div class="content" id="login">
-      <?php if(isset($message)){?>
+    <div id="menu">
+      <nav><ul>
+        <li><a href="home">Halaman Depan</a></li>
+        <li><a href="#">Tentang Kami</a></li>
+        <li class="current_page"><a href="login">Login</a></li>
+      </ul></nav>
+    </div>
+  </div>
+  <div class="content" id="login">
+    <?php if(isset($message)){?>
       <div id="infoMessage"><?php echo $message;?></div>
       <?php } ?>
       <?php echo form_open("login");?>
 
       <p>
-          <?php echo lang('login_identity_label', 'identity');?>
-          <?php echo form_input($identity);?>
+        <?php echo lang('login_identity_label', 'identity');?>
+        <?php echo form_input($identity);?>
       </p>
 
       <p>
-          <?php echo lang('login_password_label', 'password');?>
-          <?php echo form_input($password);?>
+        <?php echo lang('login_password_label', 'password');?>
+        <?php echo form_input($password);?>
       </p>
 
       <p>
-          <?php echo lang('login_remember_label', 'remember');?>
-          <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+        <?php echo lang('login_remember_label', 'remember');?>
+        <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
       </p>
 
 
@@ -54,46 +54,41 @@
             </select>
             <input type="password" placeholder="Password" name = "password"/>
 
-        </form> -->
-    </div>
+          </form> -->
+  </div>
     <div class="footer">
-        <div class="container">
-            <div class="col-md-3 footer-left">
-                <a href="index.html"><img src="<?=base_url();?>assets/img/mebelheny.png" width="100px" alt=""></a>
-                <p class="footer-class">Jalan Pangeran Diponegro No. 11 Ngawi, Jawa Timur<br>Telp. (0351) 908201</p>
-            </div>
-            <div class="col-md-2 footer-middle">
-                <ul>
-                    <li><a href="404.html">about us</a> </li>
-                    <li><a href="contact.html">   contact us</a></li>
-                    <li ><a href="products.html" >  our stores</a></li>
-                </ul>
-            </div>
-            <div class="col-md-4 footer-left-in">
-                <ul class="term">
-                    <li><a href="#">terms and conditions</a> </li>
-                    <li><a href="#">  markito in the press</a></li>
-                    <li ><a href="#" >  testimonials</a></li>
-                </ul>
-                <ul class="term">
-                    <li><a href="#">join us</a> </li>
-                    <li><a href="#">  markito videos</a></li>
-
-                </ul>
-                <div class="clearfix"> </div>
-            </div>
-            <div class="col-md-3 footer-right">
-                <h5>WE SUPPORT</h5>
-                <ul>
-                    <li><a href="#"><i> </i></a> </li>
-                    <li><a href="#"><i class="we"> </i></a></li>
-                    <li ><a href="#" ><i class="we-in"> </i></a></li>
-                    <li ><a href="#" ><i class="we-at"> </i></a></li>
-                    <li ><a href="#" ><i class="we-at-at"> </i></a></li>
-                </ul>
-            </div>
-            <div class="clearfix"> </div>
+      <div class="container">
+        <div class="col-md-3 footer-left">
+          <a href="index.html"><img src="<?=base_url();?>assets/img/mebelheny_3.png" width="150px" alt=""></a>
         </div>
+        <div class="col-md-2 footer-middle">
+          <h4><strong>Mebel Heni</strong></h4>
+          <ul>
+            <li><a href="#">Tentang Kami</a> </li>
+            <li><a href="#">Hubungi Kami</a></li>
+            <li ><a href="#">Produk</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4 footer-left-in">
+          <h4><strong>Kategori Barang</strong></h4>
+          <ul class="term">
+            <li><a href="#">Meja</a> </li>
+            <li><a href="#">Kursi</a> </li>
+            <li><a href="#">Sofa</a> </li>
+          </ul>
+          <ul class="term">
+            <li><a href="#">Lemari</a> </li>
+            <li><a href="#">Kasur</a> </li>
+            <li><a href="#">Loker</a> </li>
+          </ul>
+          <div class="clearfix"> </div>
+        </div>
+        <div class="col-md-3 footer-right">
+          <h4><strong>Alamat & Kontak</strong></h4>
+          <p class="footer-class">Jalan Sultan Agung No. 38, Ketanggi, Ngawi, Jawa Timur<br>Telp. (0351) 908201</p>
+        </div>
+        <div class="clearfix"> </div>
+      </div>
     </div>
-</body>
+  </body>
 </html>
