3fb9255d0f03913c822faf1db98cb17f8b6399fc
catris25 <blue.star95@outlook.com>
2016-06-06 21:18:25 +0700

    Add new plugin and fix some link

--- application/views/dashboard_view.php
+++ application/views/dashboard_view.php
@@ -43,8 +43,8 @@
           <li class="active"><a href="<?=base_url('dashboard');?>">Halaman Depan</a></li>
           <li><a href="<?= site_url('barang/tambah') ?>">Tambah Stok Barang</a></li>
           <li><a href="<?= site_url('barang') ?>">Lihat Stok Barang</a></li>
-          <li><a href="<?= site_url('penjualan/masuk') ?>">Rekap Barang Masuk</a></li>
-          <li><a href="<?= site_url('penjualan/keluar') ?>">Rekap Barang Keluar</a></li>
+          <li><a href="<?= site_url('barang') ?>">Rekap Barang Masuk</a></li>
+          <li><a href="<?= site_url('penjualan') ?>">Rekap Barang Keluar</a></li>
           <li><a href="<?= site_url('penjualan/catat') ?>">Rekap Laba</a></li>
           <li><a href="<?= site_url('auth/logout') ?>"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
         </ul>
@@ -62,8 +62,8 @@
         <li class="active"><a href="<?=base_url('dashboard');?>">Halaman Depan</a></li>
         <li><a href="<?= site_url('barang/tambah') ?>">Tambah Stok Barang</a></li>
         <li><a href="<?= site_url('barang') ?>">Lihat Stok Barang</a></li>
-        <li><a href="<?= site_url('penjualan/masuk') ?>">Rekap Barang Masuk</a></li>
-        <li><a href="<?= site_url('penjualan/keluar') ?>">Rekap Barang Keluar</a></li>
+        <li><a href="<?= site_url('barang') ?>">Rekap Barang Masuk</a></li>
+        <li><a href="<?= site_url('penjualan') ?>">Rekap Barang Keluar</a></li>
         <li><a href="<?= site_url('penjualan/catat') ?>">Rekap Laba</a></li>
         <li><a href="<?= site_url('auth/logout') ?>"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
       </ul><br>
